db08f301b0882e048be6d1c1e4aab01bfa3adff8
aleksandr <alexbromel@gmail.com>
2021-05-26 02:50:10 +0300

    types draft

---
+++ contracts/Types.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# options_ghc -fno-warn-orphans          #-}
+{-# options_ghc -Wno-redundant-constraints #-}
+{-# options_ghc -fno-strictness            #-}
+{-# options_ghc -fno-specialise            #-}
+
+module ErgoDex.Types where
+
+data ErgoToken = ErgoToken
+
+PlutusTx.makeIsDataIndexed ''ErgoToken [('ErgoToken, 0)]
+PlutusTx.makeLift ''ErgoToken
+
+newtype Amount a = Amount { unAmount :: Integer }
+  deriving stock   (Haskell.Show, Generic)
+  deriving newtype (ToJSON, FromJSON, ToSchema, Eq, Ord, PrintfArg)
+  deriving newtype (Haskell.Eq, Haskell.Ord, Haskell.Num)
+  deriving newtype (AdditiveGroup, AdditiveMonoid, AdditiveSemigroup, MultiplicativeSemigroup)
+PlutusTx.makeIsDataIndexed ''Amount [('Amount, 0)]
+PlutusTx.makeLift ''Amount
+
+data DexDatum = DexDatum
