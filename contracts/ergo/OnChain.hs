d750995a4d1368d430c0520706e603f5b6c544dd
aleksandr <alexbromel@gmail.com>
2021-05-29 19:04:12 +0300

    simple validation of token swap

--- contracts/ergo/OnChain.hs
+++ contracts/ergo/OnChain.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures             #-}
@@ -22,12 +23,11 @@
 {-# LANGUAGE ViewPatterns               #-}
 
 
-module OnChain where
+module ErgoDex.OnChain (checkTokenSwap) where
 
 import           Control.Monad          (void)
-import           Data.Aeson             (FromJSON, ToJSON)
 import           GHC.Generics           (Generic)
-import           Ledger                 (Address, Validator, Value)
+import           Ledger.Value           (AssetClass (..), symbols, assetClassValueOf)
 import           Ledger.Contexts        (ScriptContext(..))
 import qualified Ledger.Constraints     as Constraints
 import qualified Ledger.Typed.Scripts   as Scripts
@@ -49,16 +49,96 @@ import           Plutus.Trace.Emulator  (EmulatorTrace)
 import qualified Plutus.Trace.Emulator  as Trace
 import qualified PlutusTx
 import           PlutusTx.Prelude
-import           Ledger
-import           Schema                 (ToArgument, ToSchema)
-import           Wallet.Emulator        (Wallet (..))
-import           Types
-
-import qualified Ledger
+import Ledger
 import qualified Ledger.Ada             as Ada
 
-import qualified Data.ByteString.Char8  as C
+import qualified Prelude
+import           Schema                 (ToArgument, ToSchema)
+import           Wallet.Emulator        (Wallet (..))
+
+import ErgoDex.Types
 import qualified Prelude
 
-checkTokenSwap :: Pool -> ScriptContext -> Bool
-checkTokenSwap :: pool ctx = true
+findOwnInput' :: ScriptContext -> TxInInfo
+findOwnInput' ctx = fromMaybe (error ()) (findOwnInput ctx)
+
+valueWithin :: TxInInfo -> Value
+valueWithin = txOutValue . txInInfoResolved
+
+proxyDatumHash :: DatumHash
+proxyDatumHash = datumHashFromString "proxyDatumHash"
+
+calculateValueInOutputs :: [TxInInfo] -> Coin a -> Integer
+calculateValueInOutputs outputs coinValue =
+    foldl (getAmountAndSum) (0 :: Integer) outputs
+  where
+    getAmountAndSum :: Integer -> TxInInfo -> Integer
+    getAmountAndSum acc out = acc + unAmount (amountOf (txOutValue $ txInInfoResolved out) coinValue)
+
+ -- set correct contract datum hash
+currentContractHash :: DatumHash
+currentContractHash = datumHashFromString "dexContractDatumHash"
+
+--refactor
+
+inputsLockedByDatumHash :: DatumHash -> ScriptContext -> [TxInInfo]
+inputsLockedByDatumHash hash sCtx = [ proxyInput
+                                    | proxyInput <- txInfoInputs (scriptContextTxInfo sCtx)
+                                    , txOutDatumHash (txInInfoResolved proxyInput) == Just (hash)
+                                    ]
+
+checkTokenSwap :: ErgoDexPool -> Redeemer -> ScriptContext -> Bool
+checkTokenSwap ErgoDexPool{..} _ sCtx =
+    traceIfFalse "Expected Ergo or Ada coin to be present in input" inputContainsErgoOrAda &&
+    traceIfFalse "Expected correct value of Ergo and Ada in pool output" correctValueSwap
+  where
+
+    ownInput :: TxInInfo
+    ownInput = findOwnInput' sCtx
+
+    newOutputWithPoolContract :: TxOut
+    newOutputWithPoolContract = case [ output
+                                     | output <- getContinuingOutputs sCtx
+                                     , txOutDatumHash output == Just (snd $ ownHashes sCtx)
+                                     ] of
+      [output]   -> output
+      otherwise  -> traceError "expected exactly one output of ergo dex"
+
+    currentPoolOutput :: TxOut
+    currentPoolOutput =
+      let
+        poolInputs = inputsLockedByDatumHash currentContractHash sCtx
+      in
+        case poolInputs of
+          [input] -> txInInfoResolved input
+          otherwise -> traceError "expected exactly one input of ergo dex"
+
+    proxyInputsWithAda :: Integer
+    proxyInputsWithAda =
+      let
+        proxyInputs = inputsLockedByDatumHash proxyDatumHash sCtx
+      in calculateValueInOutputs proxyInputs adaCoin
+
+    proxyInputsWithErgo :: Integer
+    proxyInputsWithErgo =
+      let
+        proxyInputs = inputsLockedByDatumHash proxyDatumHash sCtx
+      in calculateValueInOutputs proxyInputs ergoCoin
+
+    inputContainsErgoOrAda :: Bool
+    inputContainsErgoOrAda =
+      let
+        input = valueWithin ownInput
+        containsErgo = isUnity input adaCoin
+        containsAda = isUnity input ergoCoin
+      in containsErgo || containsAda
+
+    correctValueSwap :: Bool
+    correctValueSwap =
+      let
+        currentAdaValue = assetClassValueOf (txOutValue currentPoolOutput) (unCoin adaCoin)
+        currentErgoValue = assetClassValueOf (txOutValue currentPoolOutput) (unCoin ergoCoin)
+        newAdaValue = assetClassValueOf (txOutValue newOutputWithPoolContract) (unCoin adaCoin)
+        newErgoValue = assetClassValueOf (txOutValue newOutputWithPoolContract) (unCoin ergoCoin)
+      in
+        (newErgoValue == currentErgoValue + proxyInputsWithErgo) && (newAdaValue == currentAdaValue + proxyInputsWithAda)
--- contracts/ergo/Types.hs
+++ contracts/ergo/Types.hs
@@ -17,7 +17,7 @@
 {-# options_ghc -fno-strictness            #-}
 {-# options_ghc -fno-specialise            #-}
 
-module Types where
+module ErgoDex.Types where
 
 import           Ledger
 import           Ledger.Value        (AssetClass (..), assetClass, assetClassValue, assetClassValueOf)
@@ -26,6 +26,7 @@ import qualified PlutusTx
 import           PlutusTx.Prelude
 import qualified Prelude             as Haskell
 import           Text.Printf         (PrintfArg)
+import qualified Data.ByteString.Char8  as C
 
 data ErgoToken = ErgoToken
 
@@ -50,7 +51,19 @@ newtype Amount a = Amount { unAmount :: Integer }
 PlutusTx.makeIsDataIndexed ''Amount [('Amount, 0)]
 PlutusTx.makeLift ''Amount
 
-data Pool = Pool {
-    adaValue :: Coin Ada,
-    ergoValue :: Coin ErgoToken
-}
+amountOf :: Value -> Coin a -> Amount a
+amountOf v = Amount . assetClassValueOf v . unCoin
+
+isUnity :: Value -> Coin a -> Bool
+isUnity v c = amountOf v c == 1
+
+datumHashFromString :: String -> DatumHash
+datumHashFromString str = DatumHash $ C.pack str
+
+data ErgoDexPool = ErgoDexPool {
+    adaCoin :: Coin Ada,
+    ergoCoin :: Coin ErgoToken
+} deriving (Haskell.Show, Generic, ToJSON, FromJSON, ToSchema)
+
+PlutusTx.makeIsDataIndexed ''ErgoDexPool [('ErgoDexPool, 0)]
+PlutusTx.makeLift ''ErgoDexPool
