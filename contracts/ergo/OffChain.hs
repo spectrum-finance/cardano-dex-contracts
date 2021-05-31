477784c2ba264e191501257beeaa1ef7befc610d
aleksandr <alexbromel@gmail.com>
2021-06-01 01:31:54 +0300

    add draft of offChain part

---
+++ contracts/ergo/OffChain.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# options_ghc -fno-warn-orphans          #-}
+{-# options_ghc -Wno-redundant-constraints #-}
+{-# options_ghc -fno-strictness            #-}
+{-# options_ghc -fno-specialise            #-}
+
+module ErgoDex.OffChain where
+
+import           Ledger
+import           Ledger.Value        (AssetClass (..), assetClass, assetClassValue, assetClassValueOf)
+import           Playground.Contract (FromJSON, Generic, ToJSON, ToSchema)
+import qualified PlutusTx
+import           PlutusTx.Prelude
+import qualified Prelude             as Haskell
+import           Text.Printf         (PrintfArg)
+import qualified Data.ByteString.Char8  as C
+import ErgoDex.Types
+
+data ErgoDexSwapping
+instance Scripts.ScriptType ErgoDexSwapping where
+    type instance RedeemerType ErgoDexSwapping = ContractAction
+    type instance DatumType    ErgoDexSwapping = ErgoDexPool
