7ed0c6c3bf9ec9519cfe19e456ece26ea0d49373
aleksandr <alexbromel@gmail.com>
2021-06-03 02:02:54 +0300

    refactoring

--- src/Dex/Types.hs
+++ src/Dex/Types.hs
@@ -17,22 +17,28 @@
 {-# options_ghc -fno-strictness            #-}
 {-# options_ghc -fno-specialise            #-}
 
-module ErgoDex.Types where
+module Dex.Types where
 
 import           Ledger
 import           Ledger.Value        (AssetClass (..), assetClass, assetClassValue, assetClassValueOf)
 import           Playground.Contract (FromJSON, Generic, ToJSON, ToSchema)
 import qualified PlutusTx
-import           PlutusTx.Prelude
+import PlutusTx.Prelude
+    ( Bool,
+      Integer,
+      String,
+      (.),
+      ($),
+      Eq(..),
+      AdditiveGroup,
+      AdditiveMonoid,
+      AdditiveSemigroup,
+      MultiplicativeSemigroup,
+      Ord )
 import qualified Prelude             as Haskell
 import           Text.Printf         (PrintfArg)
 import qualified Data.ByteString.Char8  as C
-
-data ErgoToken = ErgoToken
-
-data LPToken = LPToken
-
-deriving anyclass instance ToSchema AssetClass
+import           Utils
 
 PlutusTx.makeIsDataIndexed ''ErgoToken [('ErgoToken, 0)]
 PlutusTx.makeLift ''ErgoToken
@@ -40,27 +46,7 @@ PlutusTx.makeLift ''ErgoToken
 PlutusTx.makeIsDataIndexed ''LPToken [('LPToken, 0)]
 PlutusTx.makeLift ''LPToken
 
-newtype Coin a = Coin { unCoin :: AssetClass }
-  deriving stock   (Haskell.Show, Generic)
-  deriving newtype (ToJSON, FromJSON, ToSchema, Eq, Haskell.Eq, Haskell.Ord)
-PlutusTx.makeIsDataIndexed ''Coin [('Coin, 0)]
-PlutusTx.makeLift ''Coin
-
-newtype Amount a = Amount { unAmount :: Integer }
-  deriving stock   (Haskell.Show, Generic)
-  deriving newtype (ToJSON, FromJSON, ToSchema, Eq, Ord, PrintfArg)
-  deriving newtype (Haskell.Eq, Haskell.Ord, Haskell.Num)
-  deriving newtype (AdditiveGroup, AdditiveMonoid, AdditiveSemigroup, MultiplicativeSemigroup)
-PlutusTx.makeIsDataIndexed ''Amount [('Amount, 0)]
-PlutusTx.makeLift ''Amount
-
-amountOf :: Value -> Coin a -> Amount a
-amountOf v = Amount . assetClassValueOf v . unCoin
-
-isUnity :: Value -> Coin a -> Bool
-isUnity v c = amountOf v c == 1
-
-datumHashFromString :: String -> DatumHash
+datumHashFromString :: String -> DatumHash 
 datumHashFromString str = DatumHash $ C.pack str
 
 data ErgoDexPool = ErgoDexPool {
