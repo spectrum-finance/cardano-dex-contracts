3ddfb0cefe7670026fe936b24d503ef7edc92f81
oskin1 <ilya.arcadich@gmail.com>
2022-02-24 11:59:53 +0100

    WIP.

--- src/ErgoDex/PContracts/PPool.hs
+++ src/ErgoDex/PContracts/PPool.hs
@@ -8,10 +8,14 @@ import Generics.SOP (Generic, I (I))
 import Plutarch
 import Plutarch.Prelude
 import Plutarch.DataRepr
+import Plutarch.Lift
 import Plutarch.Api.V1.Contexts
-import Plutarch.Api.V1 (PValue(..))
+import Plutarch.Api.V1 (PValue(..), PTxOut)
 
+import PExtra.Monadic (tcon, tlet, tletField, tmatch, tmatchField)
 import PExtra.API
+
+import qualified ErgoDex.Contracts.Pool as P
 
 newtype PPoolConfig (s :: S) = PPoolConfig
   (
@@ -28,8 +32,11 @@ newtype PPoolConfig (s :: S) = PPoolConfig
   deriving stock (GHC.Generic)
   deriving anyclass (Generic, PIsDataRepr)
   deriving
-    (PlutusType, PIsData)
+    (PlutusType, PMatch, PIsData, PDataFields)
     via PIsDataReprInstances PPoolConfig
+
+instance PUnsafeLiftDecl PPoolConfig where type PLifted PPoolConfig = P.PoolConfig
+deriving via (DerivePConstantViaData P.PoolConfig PPoolConfig) instance (PConstant P.PoolConfig)
 
 data PPoolAction (s :: S) = PDeposit | PRedeem | PSwap
 instance PlutusType PPoolAction where
@@ -62,8 +69,24 @@ newtype PPoolState (s :: S) = PPoolState
 maxLqCap :: Term s PInteger
 maxLqCap = pconstant 0x7fffffffffffffff
 
-readPoolState :: Term s (PPoolConfig :--> PValue :--> PPoolState)
-readPoolState = plam $ \conf vl -> undefined
+readPoolState :: Term s (PPoolConfig :--> PTxOut :--> PPoolState)
+readPoolState = plam $ \conf' out -> unTermCont $ do
+  value  <- tletField @"value" out
+  poolX  <- tletField @"poolX" conf'
+  poolY  <- tletField @"poolY" conf'
+  poolLq <- tletField @"poolLq" conf'
+  let
+    x     = pdata $ assetClassValueOf # value # poolX
+    y     = pdata $ assetClassValueOf # value # poolY
+    negLq = assetClassValueOf # value # poolLq
+    lq    = pdata $ maxLqCap - negLq
+  tcon $ PPoolState
+     $ pdcons @"reservesX" @PInteger # x
+    #$ pdcons @"reservesY" @PInteger # y
+    #$ pdcons @"liquidity" @PInteger # lq
+     # pdnil
+
+-- test = pdcons @"foo" @PInteger # 7 #$ pdcons @"bar" @PInteger # 42 # pnil
 
 poolValidator :: Term s (PPoolConfig :--> PPoolAction :--> PScriptContext :--> PUnit)
 poolValidator = plam $ \datum redeemer context -> undefined 
