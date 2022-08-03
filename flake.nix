6fa7af98d3fd022c4c0e4c59571393e24c199209
mangoiv <mail@mangoiv.com>
2022-08-03 13:28:36 +0200

    cleanup: remove the old nix code, add some more commentary to the flake, adjust to the tooling that was used before, add back some stuff I temporarily removed, add hie.yaml for both the directories

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     nixpkgs.follows = "plutarch/nixpkgs";
     nixpkgs-upstream.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    # NOTE: ideally we would upgrade to the most recent iohk haskell.nix but currently 
+    #       protolude doesn't build, throwing an obscure error. Until that's fixed we're 
+    #       relying on an older version of haskell.nix 
     haskell-nix.follows = "plutarch/haskell-nix";
     haskell-nix-extra-hackage.url = "github:mlabs-haskell/haskell-nix-extra-hackage";
 
@@ -22,11 +25,12 @@
 
 
     # onchain inputs
+    # NOTE: we could now remove the `staging` part as all the
+    #       flake should stay compatible with the newest `master`
+    #       since the release of plutarch 1.2
     plutarch.url = "github:Plutonomicon/plutarch/staging";
 
     # offchain inputs
-    # plutip.url = "github:mlabs-haskell/plutip/gergely/vasil";
-
     # NOTE: this is the pendant to what was specified in the `cabal.project` file
     #       We just want the directory at the specific commit, so we set `flake = false`
     iohk-nix = {
@@ -85,19 +89,19 @@
     };
 
     iohk-monitoring-framework = {
-      url = "github:input-output-hk/iohk-monitoring-framework/46f994e216a1f8b36fe4669b47b2a7011b0e153c";
-      flake = false;
-      /*
+      /* NOTE:
         Are you thinking of updating this tag to some other commit?  Please
         ensure that the commit you are about to use is the latest one from
         the *develop* branch of this repo:
         * <https://github.com/input-output-hk/iohk-monitoring-framework/commits/develop>
         (not master!)
-      
+        
         In particular we rely on the code from this PR:
         * <https://github.com/input-output-hk/iohk-monitoring-framework/pull/622>
         being merged.
       */
+      url = "github:input-output-hk/iohk-monitoring-framework/46f994e216a1f8b36fe4669b47b2a7011b0e153c";
+      flake = false;
     };
 
     cardano-ledger = {
@@ -144,10 +148,7 @@
       supportedSystems = with nixpkgs.lib.systems.supported; tier1 ++ tier2 ++ tier3;
       perSystem = nixpkgs.lib.genAttrs supportedSystems;
 
-      fourmoluFor = system: (plainNixpkgsFor system).haskell.packages.ghc923.fourmolu;
-
-
-      # This adds 
+      # NOTE: This adds 
       # - a shellHook (pre-commit-check.shellHook) attribute you can just add to your shell with the // operator 
       #   if you add the shellHook, in your shell a new command becomes available "`pre-commit`" which you can run 
       #   by hand with `pre-commit run --all` and gets run automatically before each commit
@@ -160,19 +161,30 @@
             src = ./.;
             hooks = {
               cabal-fmt.enable = true;
-              fourmolu.enable = true;
-              markdownlint.enable = true;
+              stylish-haskell.enable = true;
               nixpkgs-fmt.enable = true;
-              shellcheck.enable = true;
-              # hlint.enable = true;
-              # statix.enable = true;
+              # FIXME: this is currently not part of the pre-commit-hooks
+              #        as the hlint currently is not happy.
+              #        Apart from that the below tools are useful but not in the 
+              #        current set of used tools
+              hlint.enable = false;
+              statix.enable = false;
+              markdownlint.enable = false;
+              shellcheck.enable = false;
             };
 
             tools = {
-              fourmolu = fourmoluFor system;
               hlint = (plainNixpkgsFor system).haskellPackages.hlint_3_4_1;
             };
           };
+
+      haskellToolsForPkgs = pkgs: [
+        pkgs.hlint
+        pkgs.cabal-install
+        pkgs.stylish-haskell
+        pkgs.ghcid
+        pkgs.haskellPackages.cabal-fmt
+      ];
 
       # ONCHAIN
       # everything that is part of the onchain project; 
@@ -219,13 +231,9 @@
               '' + (preCommitCheckFor system).shellHook;
               withHoogle = true;
               exactDeps = true;
-              nativeBuildInputs = [
-                pkgs'.cabal-install
-                pkgs'.hlint
-                pkgs'.haskellPackages.cabal-fmt
+              nativeBuildInputs = (haskellToolsForPkgs pkgs') ++ [
                 pkgs'.nixpkgs-fmt
                 plutarch.project.${system}.hsPkgs.hspec-discover.components.exes.hspec-discover
-                (fourmoluFor system)
                 (plutarch.hlsFor compiler-nix-name system)
               ];
             };
@@ -405,13 +413,8 @@
 
                 # We use the ones from Nixpkgs, since they are cached reliably.
                 # Eventually we will probably want to build these with haskell.nix.
-                nativeBuildInputs = [
-                  pkgs'.cabal-install
+                nativeBuildInputs = (haskellToolsForPkgs pkgs') ++ [
                   pkgs'.fd
-                  pkgs'.haskellPackages.apply-refact
-                  pkgs'.haskellPackages.cabal-fmt
-                  pkgs'.haskellPackages.fourmolu
-                  pkgs'.hlint
                   pkgs'.nixpkgs-fmt
 
                   #project.hsPkgs.cardano-cli.components.exes.cardano-cli
@@ -465,14 +468,9 @@
           in
           pkgs.mkShell {
             inherit (preCommitCheckFor system) shellHook;
-            nativeBuildInputs = [
-              pkgs.cabal-install
+            nativeBuildInputs = (haskellToolsForPkgs pkgs) ++ [
               pkgs.fd
-              pkgs.haskellPackages.apply-refact
-              pkgs.haskellPackages.cabal-fmt
-              pkgs.hlint
               pkgs.nixpkgs-fmt
-              (fourmoluFor system)
             ];
           };
       });
