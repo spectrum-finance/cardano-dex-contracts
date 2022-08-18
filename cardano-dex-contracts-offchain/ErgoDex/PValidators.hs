5b81a32e2a4595d648ab4deec5b12f20f3a592a8
Bromel777 <alexbromel@gmail.com>
2022-08-18 10:43:55 +0300

    PV2.Validators in PValidators

--- cardano-dex-contracts-offchain/ErgoDex/PValidators.hs
+++ cardano-dex-contracts-offchain/ErgoDex/PValidators.hs
@@ -11,36 +11,33 @@ import           Control.Monad.IO.Class
 
 import           Paths_cardano_dex_contracts_offchain
 
-import Plutus.V1.Ledger.Api
-import Plutus.V1.Ledger.Address       (scriptHashAddress)
-import Plutus.V1.Ledger.Scripts
-import qualified PlutusTx.Builtins    as Builtins
+import qualified Plutus.V2.Ledger.Api as PV2
 
 poolValidatorDataFileName :: String
 poolValidatorDataFileName = "pool.uplc"
 
-poolValidator :: (MonadIO m) => m Validator
+poolValidator :: (MonadIO m) => m PV2.Validator
 poolValidator = readValidatorFromFile poolValidatorDataFileName
 
 swapValidatorDataFileName :: String
 swapValidatorDataFileName = "swap.uplc"
 
-swapValidator :: (MonadIO m) => m Validator
+swapValidator :: (MonadIO m) => m PV2.Validator
 swapValidator = readValidatorFromFile swapValidatorDataFileName
 
 depositValidatorDataFileName :: String
 depositValidatorDataFileName = "deposit.uplc"
 
-depositValidator :: (MonadIO m) => m Validator
+depositValidator :: (MonadIO m) => m PV2.Validator
 depositValidator = readValidatorFromFile depositValidatorDataFileName
 
 redeemValidatorDataFileName :: String
 redeemValidatorDataFileName = "redeem.uplc"
 
-redeemValidator :: (MonadIO m) => m Validator
+redeemValidator :: (MonadIO m) => m PV2.Validator
 redeemValidator = readValidatorFromFile redeemValidatorDataFileName
 
-readValidatorFromFile :: (MonadIO m) => String -> m Validator
+readValidatorFromFile :: (MonadIO m) => String -> m PV2.Validator
 readValidatorFromFile dataFieldName = do
     path  <- liftIO $ getDataFileName dataFieldName
     bytes <- liftIO $ BSL.readFile path
